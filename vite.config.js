9cf398b7e78e3e53442061d952aab01d5135c812
nirnawati-expt <noviirnawati.edu@gmail.com>
2026-07-29 01:44:49 +0700

    feat: add mozilla browser build option

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,39 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import manifestBase from "./manifest.json";
 import manifestChrome from "./src/chrome/manifest.json";
+import manifestMozilla from "./src/mozilla/manifest.json";
+
+function constructManifestJsonPlugin(manifest) {
+  return {
+    name: "generate-manifest-from-json",
+    generateBundle() {
+      this.emitFile({
+        type: "asset",
+        fileName: "manifest.json",
+        source: JSON.stringify({ ...manifestBase, ...manifest }, null, 2),
+      });
+    },
+  };
+}
+
+/**
+ * Calculates the total cost.
+ * @param {string} subFolderSource - sub folder name of the source file, from /src directory
+ * @param {string} outputFolder - output folder name where the file is copied
+ * @param {string} fileName - file name and type that want to be copied, for example: 'script.js'
+ * @returns {} Object copy properties for viteStaticCopy.
+ */
+function constructStaticCopyProperties(
+  subFolderSource,
+  outputFolder,
+  fileName,
+) {
+  return {
+    src: `./${subFolderSource}/${fileName}`,
+    dest: outputFolder,
+    rename: { name: fileName, stripBase: true },
+  };
+}
 
 export default defineConfig(({ mode }) => {
   console.log("vite config mode:" + mode);
@@ -22,6 +55,7 @@ export default defineConfig(({ mode }) => {
 
   let assetsInlineLimit = 0; // all assets is moved to ${outDir}/assets
 
+  const backgroundFileName = "background.js";
   switch (mode) {
     case "static":
       plugins.push(viteSingleFile());
@@ -31,29 +65,33 @@ export default defineConfig(({ mode }) => {
       assetsInlineLimit = 10000000000;
       break;
     case "chrome-ext":
-      folder = "dist_chrome-ext";
-      plugins.push({
-        name: "generate-manifest-from-json",
-        generateBundle() {
-          this.emitFile({
-            type: "asset",
-            fileName: "manifest.json",
-            source: JSON.stringify(
-              { ...manifestBase, ...manifestChrome },
-              null,
-              2,
-            ),
-          });
-        },
-      });
+      browser_name = "chrome";
+      folder = `dist_${browser_name}`;
+      plugins.push(constructManifestJsonPlugin(manifestChrome));
       plugins.push(
         viteStaticCopy({
           targets: [
-            {
-              src: `./chrome/background.js`,
-              dest: `../${folder}`,
-              rename: { name: "background.js", stripBase: true },
-            },
+            constructStaticCopyProperties(
+              browser_name,
+              `../${folder}`,
+              backgroundFileName,
+            ),
+          ],
+        }),
+      );
+      break;
+    case "mozilla-ext":
+      browser_name = "mozilla";
+      folder = `dist_${browser_name}`;
+      plugins.push(constructManifestJsonPlugin(manifestMozilla));
+      plugins.push(
+        viteStaticCopy({
+          targets: [
+            constructStaticCopyProperties(
+              browser_name,
+              `../${folder}`,
+              backgroundFileName,
+            ),
           ],
         }),
       );
