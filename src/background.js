40cb5528b8f8b181a6eb1ae609f380c3e8b951c7
nirnawati-expt <noviirnawati.edu@gmail.com>
2026-07-28 22:51:54 +0700

    feat: add chrome browser build option

---
+++ src/background.js
@@ -0,0 +1,7 @@
+import browser from "webextension-polyfill";
+
+browser.action.onClicked.addListener(() => {
+  browser.tabs.create({
+    url: browser.runtime.getURL("index.html")
+  });
+});
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from 'path';
 import { viteSingleFile } from 'vite-plugin-singlefile';
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './manifest.json';
+import manifestChrome from './manifest.chrome.json';
 
 export default defineConfig(({ mode }) => {
   console.log("vite config mode:" + mode);
@@ -10,11 +13,13 @@ export default defineConfig(({ mode }) => {
   let plugins = [];
   let folder = "dist";
   let target = "es2020";
-  let rolldownOptions= {
-    input: {
-      main: resolve(__dirname, rootSource + '/index.html'),
-    }
+
+  const external = ['src/background.js']; // exclude background.js from build result if not building for browser extension
+  const input = {
+    main: resolve(__dirname, rootSource + '/index.html'),
   };
+  let rolldownOptions= { external, input };
+
   let assetsInlineLimit = 0; // all assets is moved to ${outDir}/assets
 
   switch (mode) {
@@ -22,8 +27,14 @@ export default defineConfig(({ mode }) => {
       plugins.push(viteSingleFile());
       target = 'es2015';
       folder = "static";
-      rolldownOptions = {};
+      rolldownOptions = { external };
       assetsInlineLimit = 10000000000;
+      break;
+    case "chrome-ext":
+      manifest.background = manifestChrome.background;
+      plugins.push(crx({ manifest }))
+      folder = "dist_chrome-ext";
+      rolldownOptions= { input };
       break;
     default:
   }
